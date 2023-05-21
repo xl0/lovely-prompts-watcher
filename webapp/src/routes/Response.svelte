05bdd6e1fb752718aad97deb02cd80b43b870a5f
Alexey <alexey.zaytsev@gmail.com>
2023-05-21 19:28:23 +0700

    Add missing files

---
+++ webapp/src/routes/Response.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+import Bubble from './Bubble.svelte';
+import { show_meta } from '$lib/utils';
+
+export let meta:object;
+export let title:string = "";
+export let comment:string =""
+
+</script>
+
+<div class="mx-2">
+  {#if title} <div class="flex justify-end"><Bubble class="translate-y-1 mx-2 max-w-fit">{title}</Bubble></div> {/if}
+
+  <div class = "border-black border rounded-md px-2 border-dashed">
+
+    {#if meta}
+    <div class="max-w-fit border-black border px-2 my-2 rounded-sm">
+      {@html show_meta(meta)}
+    </div>
+    {/if}
+
+    <p class="px-2 my-2 border border-black bg-white rounded-sm">
+      <slot></slot>
+    </p>
+  </div>
+
+  {#if comment} <div class="flex justify-end"><Bubble class="-translate-y-1 mx-2 max-w-fit">{comment}</Bubble></div> {/if}
+</div>
+
+
+
---
+++ webapp/src/routes/Summary.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import Bubble from './Bubble.svelte';
+  import { show_meta } from '$lib/utils';
+
+  export let title = "";
+  export let meta = {};
+  export let token_used = 1234;
+  export let token_max = 4000;
+  export let comment ="";
+
+</script>
+
+<div class="border-black border p-1 mx-2 my-1 rounded-md {$$props.class||''}">
+  <div class="text-base">{title}</div>
+  <div class="text-xs font-mono text-gray-500">{@html show_meta(meta)}</div>
+
+  <div class="text-xs">{comment}</div>
+
+  <div class="w-full bg-gray-200 rounded-full dark:bg-gray-700">
+    <div class="bg-blue-600 text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded-full" style="width: {100*token_used/token_max}%">{token_used}/{token_max}</div>
+  </div>
+</div>
